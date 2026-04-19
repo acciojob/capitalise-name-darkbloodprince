53840d1389ffe8daacc527a14c85d3b3f1a6e919
Dasari Vamsi <44261806+darkbloodprince@users.noreply.github.com>
2026-04-19 13:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+// Get input element
+const input = document.getElementById("fname");
+
+// Add blur event (when user leaves the field)
+input.addEventListener("blur", function () {
+  input.value = input.value.toUpperCase();
+});
